48180a36b6f887cae3ce107a97547c1f35e47c4b
caos1234matao <caos1234matao@gmail.com>
2025-10-24 17:29:57 +0200

    Configuración de directorios y aplicationproperties

---
+++ src/main/java/mrRebujito/MrRebujito/MrRebujitoApplication.java
@@ -0,0 +1,13 @@
+package mrRebujito.MrRebujito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrRebujitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrRebujitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mrRebujito/MrRebujito/MrRebujitoApplicationTests.java
@@ -0,0 +1,13 @@
+package mrRebujito.MrRebujito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrRebujitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
