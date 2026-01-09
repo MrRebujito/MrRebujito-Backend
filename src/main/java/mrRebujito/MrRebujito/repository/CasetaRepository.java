e98c3abffe25953d6766f5b1cc2a791d9af40877
juanjo210106 <jgamlop2101@g.educaand.es>
2026-01-09 09:45:34 +0100

    Método para buscar por Username - CasetaRepository.java

--- src/main/java/mrRebujito/MrRebujito/repository/CasetaRepository.java
+++ src/main/java/mrRebujito/MrRebujito/repository/CasetaRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import mrRebujito.MrRebujito.entity.Caseta;
+import java.util.Optional;
+
 
 @Repository
 public interface CasetaRepository extends JpaRepository<Caseta, Integer> {
-
+	// Método para buscar por username
+	Optional<Caseta> findByUsername(String username);
 }
