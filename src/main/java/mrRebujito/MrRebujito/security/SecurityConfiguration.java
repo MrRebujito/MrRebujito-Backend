d2c08d2583f846d72292f6f7f8b6274b1a7b4087
Joserraa05 <juke15yt@gmail.com>
2026-02-08 21:22:09 +0100

    Update SecurityConfiguration.java

--- src/main/java/mrRebujito/MrRebujito/security/SecurityConfiguration.java
+++ src/main/java/mrRebujito/MrRebujito/security/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration {
 				.requestMatchers(HttpMethod.GET, "/ayuntamiento", "/ayuntamiento/{id}").permitAll()
 
 				// 1c. Listar casetas y sus cartas (productos)
-				.requestMatchers(HttpMethod.GET, "/caseta", "/caseta/{id}", "/solicitud", "/socio").permitAll()
+				.requestMatchers(HttpMethod.GET, "/caseta", "/caseta/{id}", "/solicitud", "/solicitud/{id}", "/socio").permitAll()
 				.requestMatchers(HttpMethod.GET, "/caseta/carta/{id}", "/producto", "/producto/{id}").permitAll()
 
 				// ==========================================
